f4413771db6d1dbf40158dfa7a008ef243fd1b11
Vinicius Caldo <vcaldonoreply@github.com>
2024-12-11 23:33:02 +0100

    refactor: pass chat ID directly to uploadFile function and improve message formatting

--- bot/utils/upload.go
+++ bot/utils/upload.go
@@ -37,15 +37,17 @@ func UploadDir(ctx context.Context, b *bot.Bot, download Download) error {
 			defer file.Close()
 
 			log.Printf("Uploading file: %s\n", file.Name())
-			err = uploadFile(ctx, b, file)
+			err = uploadFile(ctx, b, file, chatIdInt)
 			if err != nil {
 				return fmt.Errorf("failed to upload file: %v", err)
 			}
 		}
 	}
+
+	humanSize := download.Size.String()
 	_, err = b.SendMessage(ctx, &bot.SendMessageParams{
 		ChatID: chatIdInt,
-		Text:   fmt.Sprintf("%s - %s", download.Name, download.Size.String()),
+		Text:   fmt.Sprintf("%s - %s", download.Name, humanSize),
 	})
 	if err != nil {
 		return fmt.Errorf("failed to send message: %v", err)
@@ -54,24 +56,14 @@ func UploadDir(ctx context.Context, b *bot.Bot, download Download) error {
 	return nil
 }
 
-func uploadFile(ctx context.Context, b *bot.Bot, file *os.File) error {
-	chatId, ok := os.LookupEnv("CHAT_ID")
-	if !ok {
-		panic("CHAT_ID env var is required")
-	}
-
-	chatIdInt, err := strconv.ParseInt(chatId, 10, 64)
-	if err != nil {
-		panic("CHAT_ID must be a valid int64")
-	}
-
+func uploadFile(ctx context.Context, b *bot.Bot, file *os.File, chatId int64) error {
 	fileReader := &models.InputFileUpload{
 		Filename: file.Name(),
 		Data:     file,
 	}
 
-	_, err = b.SendDocument(ctx, &bot.SendDocumentParams{
-		ChatID:   chatIdInt,
+	_, err := b.SendDocument(ctx, &bot.SendDocumentParams{
+		ChatID:   chatId,
 		Document: fileReader,
 	})
 	if err != nil {
