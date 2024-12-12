85eed6f01574bb796dbd2134a4ba00f54ee4a8c3
vcaldo <234766+vcaldo@users.noreply.github.com>
2024-12-13 00:24:23 +0100

    feat: add torrent info retrieval and enhance download state management in Redis (#5)
    Co-authored-by: Vinicius Caldo <vcaldonoreply@github.com>

--- jonatas/redisutils/redisutils.go
+++ jonatas/redisutils/redisutils.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"strconv"
 
 	"github.com/go-redis/redis/v8"
 )
@@ -52,9 +53,82 @@ func DownloadExistsInRedis(ctx context.Context, rdb *redis.Client, id int64) (bo
 
 func RegisterDownloadState(ctx context.Context, rdb *redis.Client, d Download) error {
 	log.Printf("Storing download in Redis: %s", d.Name)
-	err := rdb.HSet(ctx, fmt.Sprintf("%s:%d", CompletedHash, d.ID), []string{"name", d.Name, "state", d.State}).Err()
+	err := rdb.HSet(ctx, fmt.Sprintf("%s:%d", CompletedHash, d.ID), []string{NameKey, d.Name, StateKey, d.State}).Err()
 	if err != nil {
 		return fmt.Errorf("redis set failed: %w", err)
 	}
+	switch d.State {
+	case Downloaded:
+		err = rdb.SAdd(ctx, Downloaded, d.ID).Err()
+		if err != nil {
+			return fmt.Errorf("redis set failed: %w", err)
+		}
+	case Compressing:
+		err = rdb.SAdd(ctx, Compressing, d.ID).Err()
+		if err != nil {
+			return fmt.Errorf("redis set failed: %w", err)
+		}
+		err := rdb.SRem(ctx, Downloaded, d.ID).Err()
+		if err != nil {
+			return fmt.Errorf("redis set failed: %w", err)
+		}
+	case Compressed:
+		err = rdb.SAdd(ctx, Compressed, d.ID).Err()
+		if err != nil {
+			return fmt.Errorf("redis set failed: %w", err)
+		}
+		err = rdb.SRem(ctx, Compressing, d.ID).Err()
+		if err != nil {
+			return fmt.Errorf("redis set failed: %w", err)
+		}
+	case Uploading:
+		err = rdb.SAdd(ctx, Uploading, d.ID).Err()
+		if err != nil {
+			return fmt.Errorf("redis set failed: %w", err)
+		}
+		err = rdb.SRem(ctx, Compressed, d.ID).Err()
+		if err != nil {
+			return fmt.Errorf("redis set failed: %w", err)
+		}
+	case Uploaded:
+		err = rdb.SAdd(ctx, Uploaded, d.ID).Err()
+		if err != nil {
+			return fmt.Errorf("redis set failed: %w", err)
+		}
+		err = rdb.SRem(ctx, Uploading, d.ID).Err()
+		if err != nil {
+			return fmt.Errorf("redis set failed: %w", err)
+		}
+	case Removed:
+		err = rdb.SAdd(ctx, Removed, d.ID).Err()
+		if err != nil {
+			return fmt.Errorf("redis set failed: %w", err)
+		}
+		err = rdb.SRem(ctx, Uploaded, d.ID).Err()
+		if err != nil {
+			return fmt.Errorf("redis set failed: %w", err)
+		}
+	default:
+		log.Println("Unknown state")
+	}
+
 	return nil
+}
+
+func GetDowloadState(ctx context.Context, rdb *redis.Client, state string) ([]int64, error) {
+	val, err := rdb.SMembers(ctx, state).Result()
+	if err != nil {
+		return nil, fmt.Errorf("redis get failed: %w", err)
+	}
+
+	// Convert string slice to int64 slice
+	var ids []int64
+	for _, v := range val {
+		id, err := strconv.ParseInt(v, 10, 64)
+		if err != nil {
+			return nil, fmt.Errorf("failed to parse id: %w", err)
+		}
+		ids = append(ids, id)
+	}
+	return ids, nil
 }
